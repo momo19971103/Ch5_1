d6e80fbe694f3371815084126f03f455fea59dbc
momo19971103 <momo19971103@yahoo.com.tw>
2020-05-17 09:05:37 +0800

    GammaCorrection

--- Ch5_1/Ch5_1.py
+++ Ch5_1/Ch5_1.py
@@ -12,11 +12,32 @@ def img_negative(img):
     g=255-img
     return g
 
+def gamma_correction(f,gamma=2.0):
+    g = f.copy()
+    nr,nc=f.shape[:2]
+    c = 255.0/(255.0**gamma)
+    table = np.zeros(256)
+    for i in range(256):
+        table[i]=round(i**gamma*c,0)
+    if f.ndim !=3:
+        for x in range(nr):
+            for y in range(nc):
+                g[x,y]= table[f[x,y]]
+    else:
+        for x in range(nr):
+            for y in range(nc):
+                for k in range(3):
+                    g[x,y,k]= table[f[x,y,k]]
+    return g
+
+
 def main():
     imgOrg=cv2.imread("2165001.bmp",-1)
     imgNegative = img_negative(imgOrg)
+    imgGammaCorrection = gamma_correction(imgOrg,0.1)
     cv2.imshow("Original Image",imgOrg)
     cv2.imshow("Image Negative",imgNegative)
+    cv2.imshow("Gamma = 0.1",imgGammaCorrection)
     cv2.waitKey(0)
 
 main()
