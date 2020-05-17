7b3abe0a054cc5f16d4ada3ab61a576061108c3c
momo19971103 <momo19971103@yahoo.com.tw>
2020-05-17 09:37:04 +0800

    beta correction

--- Ch5_1/Ch5_1.py
+++ Ch5_1/Ch5_1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import scipy.special as special
 
 def img_negative(img):
     #b,g,r = cv2.split(img)
@@ -30,14 +31,32 @@ def gamma_correction(f,gamma=2.0):
                     g[x,y,k]= table[f[x,y,k]]
     return g
 
+def beta_correction(f,a=2.0,b=2.0):
+    g = f.copy()
+    nr,nc = f.shape[:2]
+    x = np.linspace(0,1,256)
+    table = np.round(special.betainc(a,b,x)*255,0)
+    if f.ndim!=3:
+        for x in range(nr):
+            for y in range(nc):
+                g[x,y]= table[f[x,y]]
+    else:
+        for x in range(nr):
+            for y in range(nc):
+                for k in range(3):
+                    g[x,y,k]= table[f[x,y,k]]
+    return g
+
 
 def main():
     imgOrg=cv2.imread("2165001.bmp",-1)
     imgNegative = img_negative(imgOrg)
-    imgGammaCorrection = gamma_correction(imgOrg,0.1)
+    imgGammaCorrection = gamma_correction(imgOrg,5.0)
+    imgBeta = beta_correction(imgOrg,a=5.0,b=5.0)
     cv2.imshow("Original Image",imgOrg)
     cv2.imshow("Image Negative",imgNegative)
-    cv2.imshow("Gamma = 0.1",imgGammaCorrection)
+    cv2.imshow("Gamma = 5.0",imgGammaCorrection)
+    cv2.imshow("Beta CORR(a=b=5.0)",imgBeta)
     cv2.waitKey(0)
 
 main()
